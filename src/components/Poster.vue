5b3c87c0df363e657bba4f44f4ba4f981f796813
Kim Injoong <ijboym216@gra-fik.com>
2019-05-27 14:33:28 +0900

    add Poster.vue component

---
+++ src/components/Poster.vue
@@ -0,0 +1,65 @@
+<template>
+    <router-link>
+        <div class="Container">
+            <div class="ImageContainer">
+                <div class="Image">
+
+                </div>
+                <span class="Rating">
+                    <span role="img" aria-label="rating">
+                        ⭐️
+                    </span>
+                    10/10
+                </span>
+            </div>
+            <span class="Title">
+
+            </span>
+            <span class="Year">
+
+            </span>
+        </div>
+    </router-link>
+</template>
+
+<script>
+    export default {
+        props:["id", "imageUrl", "title", "rating", "year", "isMovie"],
+        name: "Poster.vue"
+    }
+</script>
+
+<style scoped>
+    .Container{
+        font-size: 12px;
+    }
+    .Image{
+        background-image: url();
+        height: 180px;
+        background-size: cover;
+        border-radius: 4px;
+        background-position: center center;
+        transition: opacity 0.1s linear;
+    }
+    .Rating{
+        bottom: 5px;
+        right: 5px;
+        position: absolute;
+        opacity: 0;
+        transition: opacity 0.1s linear;
+    }
+    .ImageContainer{
+        margin-bottom: 5px;
+        position: relative;
+
+    }
+    .Title{
+        display: block;
+        margin-bottom: 3px;
+    }
+    .Year {
+        font-size: 10px;
+        color: rgba(255, 255, 255, 0.5);
+    }
+
+</style>
