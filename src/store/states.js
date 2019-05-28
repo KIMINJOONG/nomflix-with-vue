3aa16113612fd79199b8c8b1b5b2503d96d74b24
Kim Injoong <ijboym216@gra-fik.com>
2019-05-28 18:15:32 +0900

    state module

---
+++ src/store/states.js
@@ -0,0 +1,13 @@
+export const states = {
+    nowPlaying: null,
+    upcoming: null,
+    popular: null,
+    error: null,
+    loading: false,
+    topRated: null,
+    airingToday: null,
+    results: null,
+    tvResults: null,
+    result: null,
+    isMovie: true
+}
--- src/store/store.js
+++ src/store/store.js
@@ -2,23 +2,12 @@ import Vue from 'vue'
 import Vuex from "vuex"
 import {mutations} from "./mutations";
 import {actions} from "./actions";
+import {states} from "./states";
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-    nowPlaying: null,
-    upcoming: null,
-    popular: null,
-    error: null,
-    loading: false,
-    topRated: null,
-    airingToday: null,
-    results: null,
-    tvResults: null,
-    result: null,
-    isMovie: true
-  },
+  state: states,
   mutations: mutations,
   actions: actions
 })
